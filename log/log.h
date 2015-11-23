86b86b9a0bbfefa9fa84693d6b39e2164ed127a4
Guillermo Alvarez <alvarezgui546@gmail.com>
2015-11-23 13:36:04 -0500

    added fps counter and logging

---
+++ log/log.h
@@ -0,0 +1,18 @@
+//
+// Created by alvaregd on 23/11/15.
+//
+
+#ifndef OPENGL_CONCEPTS_LOG_H
+#define OPENGL_CONCEPTS_LOG_H
+
+
+#define GL_LOG_FILE "/home/alvaregd/Documents/Games/tutorial/gl.log"
+
+bool restart_gl_log();
+bool gl_log_err(const char* message, ...);
+bool gl_log (const char* message,...);
+
+
+#endif //OPENGL_CONCEPTS_LOG_H
+
+
--- main.cpp
+++ main.cpp
@@ -2,25 +2,132 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <assert.h>
+
+#include "log/log.h"
 
 using namespace std;
 
+int g_gl_width = 640;
+int g_gl_height = 480;
+
+void glfw_error_callback(int error, const char* description) {
+    gl_log_err("GLFW ERROR: code %i msg: %s\n", error, description);
+}
+
+void glfw_window_size_callback(GLFWwindow* window, int width, int height) {
+    g_gl_width = width;
+    g_gl_height = height;
+}
+
+void initGLFWSettings(){
+
+
+
+}
+
+void _update_fps_count(GLFWwindow * window){
+
+    static double previous_seconds = glfwGetTime();
+    static int frame_count;
+    double current_seconds = glfwGetTime();
+    double elapsed_seconds = current_seconds - previous_seconds;
+
+    if (elapsed_seconds > 0.25) {
+        previous_seconds = current_seconds;
+        double fps = (double) frame_count / elapsed_seconds;
+        char tmp[128];
+        sprintf(tmp, "opengl @fps: %.2f", fps);
+        glfwSetWindowTitle(window, tmp);
+        frame_count = 0;
+    }
+    frame_count++;
+
+}
+
+
+void log_gl_params () {
+    GLenum params[] = {
+            GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS,
+            GL_MAX_CUBE_MAP_TEXTURE_SIZE,
+            GL_MAX_DRAW_BUFFERS,
+            GL_MAX_FRAGMENT_UNIFORM_COMPONENTS,
+            GL_MAX_TEXTURE_IMAGE_UNITS,
+            GL_MAX_TEXTURE_SIZE,
+            GL_MAX_VARYING_FLOATS,
+            GL_MAX_VERTEX_ATTRIBS,
+            GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS,
+            GL_MAX_VERTEX_UNIFORM_COMPONENTS,
+            GL_MAX_VIEWPORT_DIMS,
+            GL_STEREO,
+    };
+    const char* names[] = {
+            "GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS",
+            "GL_MAX_CUBE_MAP_TEXTURE_SIZE",
+            "GL_MAX_DRAW_BUFFERS",
+            "GL_MAX_FRAGMENT_UNIFORM_COMPONENTS",
+            "GL_MAX_TEXTURE_IMAGE_UNITS",
+            "GL_MAX_TEXTURE_SIZE",
+            "GL_MAX_VARYING_FLOATS",
+            "GL_MAX_VERTEX_ATTRIBS",
+            "GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS",
+            "GL_MAX_VERTEX_UNIFORM_COMPONENTS",
+            "GL_MAX_VIEWPORT_DIMS",
+            "GL_STEREO",
+    };
+    gl_log ("GL Context Params:\n");
+    char msg[256];
+    // integers - only works if the order is 0-10 integer return types
+    for (int i = 0; i < 10; i++) {
+        int v = 0;
+        glGetIntegerv (params[i], &v);
+        gl_log ("%s %i \n", names[i], v);
+
+    }
+    // others
+    int v[2];
+    v[0] = v[1] = 0;
+    glGetIntegerv (params[10], v);
+    gl_log ("%s %i %i\n", names[10], v[0], v[1]);
+    unsigned char s = 0;
+    glGetBooleanv (params[11], &s);
+    gl_log ("%s %u\n", names[11], (unsigned int)s);
+    gl_log ("-----------------------------\n");
+}
+
+
+
 int main() {
 
-    //start the glContext and O/S window using the GLFW helper library
+    assert(restart_gl_log());
+    // log the glfw version
+    gl_log("Starting GLFW\n%s\n", glfwGetVersionString());
+    //register an error callback
+    glfwSetErrorCallback(glfw_error_callback);
 
+    //start the glContext and O/S window using the GLFW helper library
     if (!glfwInit()) {
         fprintf(stderr, "error: could not start GLFW3\n");
         return 1;
     }
 
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+
+    //note init libraries before setting hints
+    glfwWindowHint (GLFW_SAMPLES, 4);
+
+    //set to fulll screen mode
+    GLFWmonitor *mon = glfwGetPrimaryMonitor();
+    const GLFWvidmode *vmode = glfwGetVideoMode(mon);
+    GLFWwindow *window = glfwCreateWindow(g_gl_width, g_gl_height, "Extended Gl Init", NULL, NULL);
     if (!window) {
         fprintf(stderr, "ERROR:Could not open window with GLFW3\n");
         glfwTerminate();
         return 1;
     }
 
+
+    glfwSetWindowSizeCallback(window, glfw_window_size_callback);
     glfwMakeContextCurrent(window);
 
     //start glew extension handle
@@ -34,72 +141,83 @@ int main() {
 
     printf("Rendere: %s\n", renderer);
     printf("OpenGL Version Supported %s\n", version);
+    log_gl_params();
 
     //only draw if shape is close to the viewer
     glEnable(GL_DEPTH_TEST); //enable depth testing
     glDepthFunc(GL_LESS); //smaller value is closer
+    glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
 
 
     /** code goes here **/
     GLfloat points[] = {
-            0.0f,  0.5f,  0.0f,
-            0.5f, -0.5f,  0.0f,
-            -0.5f, -0.5f,  0.0f
+            0.0f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f
     };
 
     GLuint vbo = 0;
-    glGenBuffers (1, &vbo);
-    glBindBuffer (GL_ARRAY_BUFFER, vbo);
-    glBufferData (GL_ARRAY_BUFFER, sizeof (points), points, GL_STATIC_DRAW);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
     GLuint vao = 0;
-    glGenVertexArrays (1, &vao);
-    glBindVertexArray (vao);
-    glEnableVertexAttribArray (0);
-    glBindBuffer (GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-    const char* vertex_shader =
+    const char *vertex_shader =
             "#version 400\n"
                     "in vec3 vp;"
                     "void main () {"
                     "  gl_Position = vec4 (vp, 1.0);"
                     "}";
 
-    const char* fragment_shader =
+    const char *fragment_shader =
             "#version 400\n"
                     "out vec4 frag_colour;"
                     "void main () {"
-                    "  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+                    "  frag_colour = vec4 (1, 0.0, 0.5, 1.0);"
                     "}";
 
-    GLuint vs = glCreateShader (GL_VERTEX_SHADER);
-    glShaderSource (vs, 1, &vertex_shader, NULL);
-    glCompileShader (vs);
-    GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
-    glShaderSource (fs, 1, &fragment_shader, NULL);
-    glCompileShader (fs);
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
 
+    GLuint shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+    glLinkProgram(shader_programme);
 
-    GLuint shader_programme = glCreateProgram ();
-    glAttachShader (shader_programme, fs);
-    glAttachShader (shader_programme, vs);
-    glLinkProgram (shader_programme);
-
-    while (!glfwWindowShouldClose (window)) {
+    while (!glfwWindowShouldClose(window)) {
+        _update_fps_count(window);
         // wipe the drawing surface clear
-        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram (shader_programme);
-        glBindVertexArray (vao);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glViewport(0, 0, g_gl_width, g_gl_height);
+
+
+        glUseProgram(shader_programme);
+        glBindVertexArray(vao);
         // draw points 0-3 from the currently bound VAO with current in-use shader
-        glDrawArrays (GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         // update other events like input handling
-        glfwPollEvents ();
+        glfwPollEvents();
         // put the stuff we've been drawing onto the display
-        glfwSwapBuffers (window);
+        glfwSwapBuffers(window);
+
+        if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+            glfwSetWindowShouldClose(window, 1);
+        }
     }
 
     glfwTerminate();
     cout << "Hello, World!" << endl;
     return 0;
 }
+
+
