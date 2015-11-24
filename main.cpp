01cfc7f9d8f67ca2cca662dd498135f94ce664ac
Guillermo Alvarez <alvarezgui546@gmail.com>
2015-11-23 20:55:52 -0500

    back camera movement and  math libraries

--- main.cpp
+++ main.cpp
@@ -5,22 +5,30 @@
 #include <fstream>
 #include <assert.h>
 #include <cmath>
+#include <utils/maths_funcs.h>
 
 #include "log/log.h"
 
 using namespace std;
 
-int g_gl_width = 640;
-int g_gl_height = 480;
 
+struct Camera{
+
+    float speed = 1.0f;
+    float yaw_speed = 10.0f;
+    float pos[3] = {0.0f, 0.0f, 2.0f}; //don't start at zero or camera will be too closes
+    float yaw =0.0f; //y-rotation in deg
+
+};
+
+static double elapsed_seconds;
+static Camera camera;
 
 void glfw_error_callback(int error, const char* description) {
     gl_log_err("GLFW ERROR: code %i msg: %s\n", error, description);
 }
 
 void glfw_window_size_callback(GLFWwindow* window, int width, int height) {
-    g_gl_width = width;
-    g_gl_height = height;
 }
 
 
@@ -32,7 +40,7 @@ void _update_fps_count(GLFWwindow * window){
     static double previous_seconds = glfwGetTime();
     static int frame_count;
     double current_seconds = glfwGetTime();
-    double elapsed_seconds = current_seconds - previous_seconds;
+    elapsed_seconds = current_seconds - previous_seconds;
 
     if (elapsed_seconds > 0.25) {
         previous_seconds = current_seconds;
@@ -66,7 +74,7 @@ int main() {
     //set to fulll screen mode
     GLFWmonitor *mon = glfwGetPrimaryMonitor();
     const GLFWvidmode *vmode = glfwGetVideoMode(mon);
-    GLFWwindow *window = glfwCreateWindow(g_gl_width, g_gl_height, "Extended Gl Init", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(vmode->width, vmode->height, "Extended Gl Init", mon, NULL);
     if (!window) {
         fprintf(stderr, "ERROR:Could not open window with GLFW3\n");
         glfwTerminate();
@@ -95,9 +103,9 @@ int main() {
     glDepthFunc(GL_LESS); //smaller value is closer
 //    glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
 
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glFrontFace(GL_CW);
+//    glEnable(GL_CULL_FACE);
+//    glCullFace(GL_BACK);
+//    glFrontFace(GL_CW);
 
 
 
@@ -113,7 +121,6 @@ int main() {
             0.0f, 1.0f,  0.0f,
             0.0f, 0.0f,  1.0f
     };
-
 
 
     GLuint points_vbo = 0;
@@ -142,11 +149,13 @@ int main() {
                     "in vec3 vertex_position;"
                     "in vec3 vertex_colour;"
 
+                    "uniform mat4 view, proj;"
                     " out vec3 colour;"
 
                     "void main () {"
                     "  colour = vertex_colour;"
-                    "  gl_Position =vec4 (vertex_position, 1.0);"
+                    "  gl_Position =proj * view * vec4 (vertex_position, 1.0);"
+//                    "  gl_Position =proj * view * model * vec4 (vertex_position, 1.0);"
                     "}";
 
     const char *fragment_shader =
@@ -191,17 +200,46 @@ int main() {
         _print_programme_info_log(shader_programme);
     }
 
+    //create the view mtriex
+    mat4 T = translate(identity_mat4(), vec3(-camera.pos[0], -camera.pos[1], -camera.pos[2]));
+    mat4 R = rotate_y_deg(identity_mat4(), -camera.yaw);
+    mat4 view_mat = R * T;
+
+    float near = 0.1f; //clipping plane
+    float far = 100.f; //clipping plane
+    float fov = 67.0f * ONE_DEG_IN_RAD;
+    float aspect = (float) vmode->width / (float) vmode->height;
+
+    //creating a projection matrix
+    float range = tan(fov * 0.5f) * near;
+    float Sx = (2.0f * near) / (range * aspect + range * aspect);
+    float Sy = near / range;
+    float Sz = -(far + near) / (far - near);
+    float Pz = -(2.0f * far * near) / (far - near);
+
+    float proj_mat[] = {
+            Sx, 0.0f, 0.0f, 0.0f,
+            0.0f, Sy, 0.0f, 0.0f,
+            0.0f, 0.0f, Sz, -1.0f,
+            0.0f, 0.0f, Pz, 0.0f
+    };
+
+
+    int view_mat_location = glGetUniformLocation(shader_programme, "view");
+    int proj_mat_location = glGetUniformLocation(shader_programme, "proj");
+
+
+    glUseProgram(shader_programme);
+    glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, view_mat.m);
+    glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, proj_mat);
 
     while (!glfwWindowShouldClose(window)) {
 
-        //calculate movement
-
-
         _update_fps_count(window);
-        // wipe the drawing surface clear
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shader_programme);
+
         glBindVertexArray(vao);
         // draw points 0-3 from the currently bound VAO with current in-use shader
         glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -212,6 +250,49 @@ int main() {
 
         if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)) {
             glfwSetWindowShouldClose(window, 1);
+        }
+
+        elapsed_seconds = elapsed_seconds/10;
+
+        bool cam_moved = false;
+        if (glfwGetKey (window, GLFW_KEY_A)) {
+            camera.pos[0] -= camera.speed * elapsed_seconds;
+            cam_moved = true;
+        }
+        if (glfwGetKey (window, GLFW_KEY_D)) {
+            camera.pos[0] += camera.speed* elapsed_seconds;
+            cam_moved = true;
+        }
+        if (glfwGetKey (window, GLFW_KEY_PAGE_UP)) {
+            camera.pos[1] += camera.speed * elapsed_seconds;
+            cam_moved = true;
+        }
+        if (glfwGetKey (window, GLFW_KEY_PAGE_DOWN)) {
+            camera.pos[1] -= camera.speed* elapsed_seconds;
+            cam_moved = true;
+        }
+        if (glfwGetKey (window, GLFW_KEY_W)) {
+            camera.pos[2] -= camera.speed * elapsed_seconds;
+            cam_moved = true;
+        }
+        if (glfwGetKey (window, GLFW_KEY_S)) {
+            camera.pos[2] += camera.speed * elapsed_seconds;
+            cam_moved = true;
+        }
+        if (glfwGetKey (window, GLFW_KEY_LEFT)) {
+            camera.yaw += camera.yaw_speed * elapsed_seconds;
+            cam_moved = true;
+        }
+        if (glfwGetKey (window, GLFW_KEY_RIGHT)) {
+            camera.yaw -= camera.yaw_speed* elapsed_seconds;
+            cam_moved = true;
+        }
+// update view matrix
+        if (cam_moved) {
+            mat4 T = translate (identity_mat4 (), vec3 (-camera.pos[0], -camera.pos[1], -camera.pos[2])); // cam translation
+            mat4 R = rotate_y_deg (identity_mat4 (), -camera.yaw); //
+            mat4 view_mat = R * T;
+            glUniformMatrix4fv (view_mat_location, 1, GL_FALSE, view_mat.m);
         }
     }
 
